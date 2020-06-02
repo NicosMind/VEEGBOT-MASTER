e34abfc7f9ad54a2bed07e26491994e2bfc51cd3
NicosMind <nico_drift@hotmail.fr>
2020-06-02 11:10:10 +0200

    Ajoutez des fichiers projet.

---
+++ VEEGBOT MASTER/VEEGBOT MASTER.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		VEEGBOT_MASTER.ino
+ Created:	02/06/2020 11:09:56
+ Author:	nico_
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
